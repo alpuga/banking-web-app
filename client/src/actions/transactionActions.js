bd6f423bb54ab0105b3bd2abf363775be7b197db
Alex <alpuga8@gmail.com>
2019-04-02 13:35:57 -0400

    Account Actions & Transaction Actions

---
+++ client/src/actions/transactionActions.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import {
+  ACCOUNTS_LOADING,
+  GET_TRANSACTIONS,
+  TRANSACTIONS_LOADING
+} from "./types";
+
+// Get Transactions
+export const getTransactions = plaidData => dispatch => {
+  dispatch(setTransactionsLoading());
+  axios
+    .post("/api/plaid/accounts/transactions", plaidData)
+    .then(res =>
+      dispatch({
+        type: GET_TRANSACTIONS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_TRANSACTIONS,
+        payload: null
+      })
+    );
+};
+// Transactions loading
+export const setTransactionsLoading = () => {
+  return {
+    type: TRANSACTIONS_LOADING
+  };
+};
