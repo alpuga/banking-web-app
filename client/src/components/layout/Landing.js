a8687077afb36c545d6751ab7dd9eb8da8d6293a
Alex <alpuga8@gmail.com>
2019-04-02 15:33:45 -0400

    Button Change

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -47,7 +47,7 @@ class Landing extends Component {
                   borderRadius: "3px",
                   letterSpacing: "1.5px"
                 }}
-                className="btn btn-large btn-flat waves-effect white black-text"
+                className="btn btn-large waves-effect waves-light hoverable white black-text"
               >
                 Log In
               </Link>
