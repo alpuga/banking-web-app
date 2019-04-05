b1d6063569c44d3302cdb57aa84c4e5e25a54638
Alex <alpuga8@gmail.com>
2019-04-04 22:53:07 -0400

    Prepared Deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const plaid = require("./routes/api/plaid");
@@ -28,6 +29,8 @@ mongoose.connect(
   }
 );
 
+const port = process.env.PORT || 5000;
+
 //Passport middleware
 app.use(passport.initialize());
 
@@ -38,7 +41,15 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/plaid", plaid);
 
-const port = process.env.PORT || 5000;
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log("Server running on port: " + port);
