cd17abc2356c1164e359d3b692a94f5ebbb920c0
Alex <alpuga8@gmail.com>
2019-08-23 15:10:48 -0400

    Updated Landing Page

--- routes/api/plaid.js
+++ routes/api/plaid.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const plaid = require("plaid");
+const express = require('express');
+const plaid = require('plaid');
 const router = express.Router();
-const passport = require("passport");
-const moment = require("moment");
-const mongoose = require("mongoose");
+const passport = require('passport');
+const moment = require('moment');
+const mongoose = require('mongoose');
 //const plaidKeys = require("../../config/keys_plaid");
 
 // Load Account and User models
-const Account = require("../../models/Account");
-const User = require("../../models/User");
-const PLAID_CLIENT_ID = "5ca3743dc41ed900147a366c"; // YOUR_CLIENT_ID plaidKeys.plaidClientId
-const PLAID_SECRET = "d0c250e4213292e72894e339449be9"; // YOUR_SECRET plaidKeys.plaidSecret
-const PLAID_PUBLIC_KEY = "4508d464022e7606f19a772439b37c"; //YOUR_PUBLIC_KEY plaidKeys.plaidPublicKey
+const Account = require('../../models/Account');
+const User = require('../../models/User');
+const PLAID_CLIENT_ID = '5ca3743dc41ed900147a366c'; // YOUR_CLIENT_ID plaidKeys.plaidClientId
+const PLAID_SECRET = 'd0c250e4213292e72894e339449be9'; // YOUR_SECRET plaidKeys.plaidSecret
+const PLAID_PUBLIC_KEY = '4508d464022e7606f19a772439b37c'; //YOUR_PUBLIC_KEY plaidKeys.plaidPublicKey
 const client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
   PLAID_PUBLIC_KEY,
   plaid.environments.sandbox,
-  { version: "2018-05-22" }
+  { version: '2019-05-29' }
 );
 
 var PUBLIC_TOKEN = null;
@@ -28,8 +28,8 @@ var ITEM_ID = null;
 // @desc Trades public token for access token and stores credentials in database
 // @access Private
 router.post(
-  "/accounts/add",
-  passport.authenticate("jwt", { session: false }),
+  '/accounts/add',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     PUBLIC_TOKEN = req.body.public_token;
     const userId = req.user.id;
@@ -50,7 +50,7 @@ router.post(
           })
             .then(account => {
               if (account) {
-                console.log("Account already exists");
+                console.log('Account already exists');
               } else {
                 const newAccount = new Account({
                   userId: userId,
@@ -77,8 +77,8 @@ router.post(
 // @desc Delete account with given id
 // @access Private
 router.delete(
-  "/accounts/:id",
-  passport.authenticate("jwt", { session: false }),
+  '/accounts/:id',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Account.findById(req.params.id).then(account => {
       // Delete account
@@ -91,8 +91,8 @@ router.delete(
 // @desc Get all accounts linked with plaid for a specific user
 // @access Private
 router.get(
-  "/accounts",
-  passport.authenticate("jwt", { session: false }),
+  '/accounts',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Account.find({ userId: req.user.id })
       .then(accounts => res.json(accounts))
@@ -104,12 +104,12 @@ router.get(
 // @desc Fetch transactions from past 30 days from all linked accounts
 // @access Private
 router.post(
-  "/accounts/transactions",
-  passport.authenticate("jwt", { session: false }),
+  '/accounts/transactions',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const now = moment();
-    const today = now.format("YYYY-MM-DD");
-    const thirtyDaysAgo = now.subtract(30, "days").format("YYYY-MM-DD");
+    const today = now.format('YYYY-MM-DD');
+    const thirtyDaysAgo = now.subtract(30, 'days').format('YYYY-MM-DD');
 
     let transactions = [];
 
